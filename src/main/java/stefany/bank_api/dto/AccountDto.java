d83bff21fd277b1e4290da80700c1bb386f21ad1
steefanyalvees <stefanyanjoos@hotmail.com>
2026-04-01 16:05:06 +0100

    implementing  mapper

--- src/main/java/stefany/bank_api/dto/AccountDto.java
+++ src/main/java/stefany/bank_api/dto/AccountDto.java
@@ -1,10 +1,14 @@
 package stefany.bank_api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class AccountDto {
     private Long id;
     private String accountHolderName;
     private double balance;
+
+
 }
--- src/main/java/stefany/bank_api/mapper/AccountMapper.java
+++ src/main/java/stefany/bank_api/mapper/AccountMapper.java
@@ -1,4 +1,23 @@
 package stefany.bank_api.mapper;
 
+import stefany.bank_api.dto.AccountDto;
+import stefany.bank_api.entity.Account;
+
 public class AccountMapper {
+
+    public static Account mapToAccount(AccountDto accountDto){
+        Account account = new Account(
+                accountDto.getId(),
+                accountDto.getAccountHolderName(),
+                accountDto.getBalance());
+     return account;
+    }
+    public static AccountDto  mapToAccountDto(Account account){
+        AccountDto accountDto = new AccountDto(
+                account.getId(),
+                account.getAccountHolderName(),
+                account.getBalance()
+        );
+        return accountDto;
+    }
 }
