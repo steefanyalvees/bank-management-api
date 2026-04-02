6e3c97cf157d70b7ea0ce5b8320f5123f584cc45
steefanyalvees <stefanyanjoos@hotmail.com>
2026-04-02 14:59:29 +0100

    create withdraw

--- src/main/java/stefany/bank_api/controller/AccountController.java
+++ src/main/java/stefany/bank_api/controller/AccountController.java
@@ -29,11 +29,18 @@ public class AccountController {
     }
     @PutMapping("/{id}/deposit")
         public ResponseEntity<AccountDto>deposit(@PathVariable Long id,
-                                                 @RequestBody Map<String,Double> request){
+                                                 @RequestBody Map<String,Double> request) {
         double amount = request.get("amount");
-        AccountDto accountDto = accountService.deposit(id,amount);
+        AccountDto accountDto = accountService.deposit(id, amount);
         return ResponseEntity.ok(accountDto);
-
+    }
+        @PutMapping("{id}/withdraw")
+        public ResponseEntity<AccountDto> withdraw(@PathVariable Long id,
+                                                   @RequestBody Map<String,Double> request){
+        double amount = request.get("amount");
+        AccountDto accountDto = accountService.withdraw(id,amount);
+        return ResponseEntity.ok(accountDto)
+        ;
     }
 
 }
